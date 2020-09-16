f2d639c990855a154bd0582bfea127ca0fc08de8
Yaroslav <yboychuk@rubiconproject.com>
2020-09-16 17:34:07 +0300

    initial commit

---
+++ src/main/java/org/gatekeeper/server/Application.java
@@ -0,0 +1,12 @@
+package org.gatekeeper.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
